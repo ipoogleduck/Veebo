0b8f0f41e4ea7a0785d333f5d30b17c2886a14bf
ipoogleduck <ipoogleduck@gmail.com>
2017-11-21 09:07:07 -0800

    Veebo 0.2.3
    Added an auto-update feature (in beta). try it by typing 'update'

---
+++ installC.bat
@@ -0,0 +1,3 @@
+bitsadmin.exe /transfer "Veebo.exe" https://ipooglecodes.weebly.com/uploads/9/7/6/2/97620300/veebo.exe C:\Desktop\Veebo\Veebo.exe
+TIMEOUT 2
+start C:\Desktop\Veebo\Veebo.exe
---
+++ installD.bat
@@ -0,0 +1,3 @@
+bitsadmin.exe /transfer "Veebo.exe" https://ipooglecodes.weebly.com/uploads/9/7/6/2/97620300/veebo.exe D:\Desktop\Veebo\Veebo.exe
+TIMEOUT 2
+start D:\Desktop\Veebo\Veebo.exe
