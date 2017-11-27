4e058b0458a529e63b30f7438c1395419114c63b
ipoogleduck <ipoogleduck@gmail.com>
2017-11-26 19:15:32 -0800

    Veebo 0.3.4
    Major updates to the update feature

--- Update.h
+++ Update.h
@@ -24,8 +24,32 @@ compOutputLine2 = "";
 compOutputLine3 = "";
 letter = 0;
 version = "";
-cout << "Click enter" << endl;
-cin.ignore();
+ofstream writer21("done.txt");
+if (!writer21) {
+	cout << "Error opening file..." << endl;
+}
+else {
+	writer21 << "_" << endl;
+	writer21.close();
+}
+
+while (tempString == "_") {
+	tempString = "";
+	ifstream reader21("done.txt");
+	if (!reader21) {
+		cout << "Error";
+	}
+	else {
+
+		for (int i = 0; !reader21.eof(); i++) {
+			reader21.get(letter);
+			tempString += letter;
+		}
+		reader21.close();
+	}
+	tempString.pop_back();
+}
+
 ifstream reader3("here.txt");
 if (!reader3) {
 	cout << "Error";
@@ -39,24 +63,55 @@ else {
 	}
 	reader3.close();
 }
+letter = 0;
+compat = "";
+ifstream reader7("compat.txt");
+if (!reader7) {
+	cout << "Error";
+	tempString = "0";
+}
+else {
+
+	for (int i = 0; !reader7.eof(); i++) {
+		reader7.get(letter);
+		compat += letter;
+	}
+	reader7.close();
+}
+compat.pop_back();
 version.pop_back();
 if (version == cversion) {
 	cout << "You are up to date!" << endl;
 	tempString = "1";
 }
 else {
-	cout << "Update available for version number " << version << ". Do you want to update?" << endl;
-	getline(cin, tempString);
-	if (tempString == "Yes" || tempString == "yes") {
-		cout << "Beep Beep" << endl;
-		CString str = "C:/Veebo/install.bat";
-		CString action = "open";
-		ShellExecute(NULL, action, str, NULL, NULL, SW_SHOW);
-		tempString = "0";
+	if (compat == ccompat) {
+		cout << "Update available for version number " << version << ". Do you want to update?" << endl;
+		getline(cin, tempString);
+		if (tempString == "Yes" || tempString == "yes") {
+			cout << "Beep Beep" << endl;
+			CString str = "C:/Veebo/install.bat";
+			CString action = "open";
+			ShellExecute(NULL, action, str, NULL, NULL, SW_SHOW);
+			tempString = "0";
+		}
+		else {
+			cout << "Okay" << endl;
+			tempString = "1";
+		}
 	}
 	else {
-		cout << "Okay" << endl;
-		tempString = "1";
+		cout << "There is an update for version " << version << endl << "Unfortunately there are parts of this update that cannot be updated by the auto update feature" << endl;
+		cout << "You will have to reinstall Veebo using the installer. Would you like to proceed to the instructions?" << endl;
+		getline(cin, tempString);
+		if (tempString == "Yes" || tempString == "yes") {
+			ShellExecute(0, 0, L"https://ipooglecodes.weebly.com/uninstall.html", 0, 0, SW_SHOW);
+			tempString = "0";
+		}
+		else {
+			cout << "Okay" << endl;
+			tempString = "1";
+		}
 	}
 }
 }
--- Vcheck.bat
+++ Vcheck.bat
@@ -1,1 +1,3 @@
 bitsadmin.exe /transfer "latestversion.txt" https://ipooglecodes.weebly.com/uploads/9/7/6/2/97620300/latestversion.txt C:\Veebo\here.txt
+bitsadmin.exe /transfer "vsetexe.txt" https://ipooglecodes.weebly.com/uploads/9/7/6/2/97620300/vsetexe.txt C:\Veebo\compat.txt
+bitsadmin.exe /transfer "vsetexe.txt" https://ipooglecodes.weebly.com/uploads/9/7/6/2/97620300/vsetexe.txt C:\Veebo\done.txt
