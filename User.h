f792b1260ab0a4c1fa8ece6374df78614a4f625f
ipoogleduck <ipoogleduck@gmail.com>
2017-11-29 17:06:36 -0800

    Veebo 0.3.7
    Prep for Veebo 0.4

---
+++ User.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "stdafx.h"
+#include <stdio.h>
+#include <cstdlib>
+#include <Windows.h> // use < > for all system and library headers
+#include <winuser.h>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <complex>
+#include <string>
+#include <ctime>
+#include <fstream>
+#include <atlstr.h>
+using namespace std;
+
+void addUser() {
+
+}
+void removeUser() {
+
+}
+void switchUser() {
+
+}
